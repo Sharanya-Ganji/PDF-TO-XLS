64e6181e2a1cef8abf431ae98b5db3a51520e5ac
Sharanya-Ganji <112933974+Sharanya-Ganji@users.noreply.github.com>
2023-07-03 10:55:16 +0530

    Add files via upload

---
+++ ExcelDownload.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Form from './form';
+import '../App.css';
+import image1 from './3rd screen.png';
+
+export default function View() {
+    
+    const handleClick = async () => {
+        const filePath = process.env.PUBLIC_URL + '/dummy.xlsx'; 
+        try {
+            const response = await fetch(filePath);
+            const fileData = await response.blob();
+            const file = new File([fileData], 'dummy.xlsx', { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+            const fileUrl = URL.createObjectURL(file);
+            const link = document.createElement('a');
+            link.href = fileUrl;
+            link.download = 'dummy.xlsx';
+            link.click();
+            console.log("downloaded");
+        }catch(err){
+            console.log("There is an error");
+        }
+        
+    };
+    return (
+        
+        <div className='container'>
+            <p style={{display:"flex", justifyContent:"center"}}>Review Document</p>
+            <div className='pdf-image'
+            style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      >
+            <img className="view-img" src={image1} alt='pdf' style={{ marginRight: '100px', height:'500px', width:'60%' }}></img>
+            <Form />
+            </div>
+            <button className='btn' onClick={handleClick}>Download AP Bookings</button>
+        </div>
+    );
+}
---
+++ View.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {  useNavigate } from 'react-router-dom';
+import '../App.css';
+
+import image1 from './2nd screen.png';
+import image2 from './view-img.png'
+
+export default function View() {
+    const navigator = useNavigate();
+    function handleClick() {
+      console.log('extract data');
+        navigator('/exceldownload')
+    };
+    return (
+        
+        <div className='container'>
+        <p style={{display:"flex", justifyContent:"center"}}>Process Document</p>
+            <div className='pdf-image'
+            style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+      }}
+      >
+               <img src={image1} className="view-img" alt='pdf' style={{ marginRight: '100px', height:'450px' }}></img><br></br>
+               <img src={image2} className="view-img" alt='pdf' style={{height:'450px'}}></img>
+
+            </div>
+            <button className='btn' onClick={handleClick}>Extract Data</button>
+        </div>
+    );
+}
