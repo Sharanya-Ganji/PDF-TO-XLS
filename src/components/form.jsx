7fc2ab7a3480179d3f51a4353ac72c4c50fa6461
Sharanya-Ganji <112933974+Sharanya-Ganji@users.noreply.github.com>
2023-07-04 13:36:05 +0530

    Update form.jsx

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import '../App.css';
+import styled from 'styled-components';
 import Radio from './Radio';
-import CheckBox1 from './Checkbox1';
-import CheckBox2 from './Checkbox2';
+import {Checkbox1, Checkbox2} from './Checkbox';
+
+
+const FormData=styled.div`
+border-radius: 10px;
+box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+border: solid 0.5px;
+display: flex;
+flex-direction: column;
+font-size: small;
+height: 500px;
+width: 40%;
+padding: 20px;
+line-height: 20px;
+color: black;
+`;
+
+const FormInp=styled.input`
+  padding: 8px;
+  width: 250px;
+  border:0.5px solid rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  outline: none;
+  &:hover {
+    border-color: rgba(0, 0, 0, 0.87);
+    box-sizing: border-box;
+    transition: color 0.2s ease;  
+  }
+
+  @media (max-width: 1000px) {
+    width: 150px;
+  }
+`;
 
 
 const Form = () => {
@@ -13,53 +45,60 @@ const Form = () => {
   };
 
   return (
-    <div className='form-data'>
+    <FormData>
     <form  onSubmit={handleSubmit}>
         <Radio/>
       <label>
         <table>
             <td>Full Name:</td>
-            <td><CheckBox1/></td>
+            <td><Checkbox1/></td>
         </table>
-        <input className="form-inp" type="text"  />
-      </label> <CheckBox2/>
+        <FormInp type="text"  />
+      </label>
       <br />
+      <Checkbox2/>
+      <br/><br/>
       <Radio/>
       <label>
         <table>
             <td>Phone number:</td>
-            <td><CheckBox1/></td>
+            <td><Checkbox1/></td>
         </table>
-        <input className="form-inp"
+        <FormInp
           type="text"
         />
-      </label> <CheckBox2/>
+      </label> 
       <br />
+      <Checkbox2/>
+      <br/><br/>
+
       <Radio/>
       <label>
         <table>
             <td>Home address:</td>
-            <td><CheckBox1/></td>
+            <td><Checkbox1/></td>
         </table>
-        <input className="form-inp"
+        <FormInp
           type="text"
         />
-      </label> <CheckBox2/>
+      </label>
       <br />
+      <Checkbox2/>
+      <br/><br/>
       <Radio/>
       <label>
       <table>
             <td>Email:</td>
-            <td><CheckBox1/></td>
+            <td><Checkbox1/></td>
         </table>
-        <input className="form-inp"
+        <FormInp
           type="email"
         />
-      </label>
-      <CheckBox2/>
+      </label><br/>
+      <Checkbox2/>
       
     </form>
-    </div>
+    </FormData>
   );
 };
 
